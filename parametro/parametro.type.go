af34386d4e6af954e5414bedfaff6b1619be10ac
Dyego Cruz <dyegocruz@gmail.com>
2022-06-29 17:06:25 -0300

    Change form of dataload.

--- parametro/parametro.type.go
+++ parametro/parametro.type.go
@@ -10,3 +10,12 @@ type Parametro struct {
 	Tipo    string  `bson:"tipo"`
 	Options Options `bson:"options"`
 }
+
+// {
+//   "tipo": "CARGA_TMDB_CONFIG",
+//   "options": {
+//     "tmdbHost": "https://api.themoviedb.org/3",
+//     "tmdbApiKey": "26fe6f55e55736490dee0811901cccac",
+//     "tmdbMaxPageLoad": 20
+//   }
+// }
