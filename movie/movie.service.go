ea22e745b80e8264797ce85cdd15c9b51a2bf3e5
Dyego Cruz <dyegocruz@gmail.com>
2022-07-01 09:10:38 -0300

    Testing loading data by files.

--- movie/movie.service.go
+++ movie/movie.service.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"moviedb/database"
 	"moviedb/parametro"
-	"moviedb/person"
 	"net/http"
 	"os"
 	"strconv"
@@ -16,6 +15,8 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+var movieCollection = "movie"
 
 func GetMovieDetailsOnApiDb(id int, language string) Movie {
 	parametro := parametro.GetByTipo("CARGA_TMDB_CONFIG")
@@ -50,53 +51,55 @@ func PopulateMovieByLanguage(itemObj Movie, language string) {
 
 	json.NewDecoder(reqCredits.Body).Decode(&itemObj.MovieCredits)
 
-	for _, cast := range itemObj.MovieCredits.Cast {
+	// for _, cast := range itemObj.MovieCredits.Cast {
 
-		var personCheck person.Person
-		personCheck.Id = cast.Id
-		personCheck.Name = cast.Name
-		// personCheck.OriginalName = cast.OriginalName
-		personCheck.KnowForDepartment = cast.KnownForDepartment
-		personCheck.Language = language
-		personCheck.Slug = slug.Make(personCheck.Name)
-		personCheck.SlugUrl = "person-" + strconv.Itoa(personCheck.Id)
+	// 	var personCheck person.Person
+	// 	personCheck.Id = cast.Id
+	// 	personCheck.Name = cast.Name
+	// 	// personCheck.OriginalName = cast.OriginalName
+	// 	personCheck.KnowForDepartment = cast.KnownForDepartment
+	// 	personCheck.Language = language
+	// 	personCheck.Slug = slug.Make(personCheck.Name)
+	// 	personCheck.SlugUrl = "person-" + strconv.Itoa(personCheck.Id)
 
-		// personFindUpdate := person.GetPersonByIdAndLanguage(cast.Id, language)
+	// 	// personFindUpdate := person.GetPersonByIdAndLanguage(cast.Id, language)
 
-		person.PopulatePersonByLanguage(personCheck, language)
-		// if personFindUpdate.Id == 0 {
-		// 	log.Println("TREAT PERSON CAST: ", personCheck.Id)
-		// 	person.PopulatePersonByLanguage(personCheck, language)
-		// 	// person.InsertPerson(personCheck)
-		// }
-	}
+	// 	person.PopulatePersonByLanguage(personCheck, language)
+	// 	// if personFindUpdate.Id == 0 {
+	// 	// 	log.Println("TREAT PERSON CAST: ", personCheck.Id)
+	// 	// 	person.PopulatePersonByLanguage(personCheck, language)
+	// 	// 	// person.InsertPerson(personCheck)
+	// 	// }
+	// }
 
-	for _, crew := range itemObj.MovieCredits.Crew {
+	// for _, crew := range itemObj.MovieCredits.Crew {
 
-		var personCheck person.Person
-		personCheck.Id = crew.Id
-		personCheck.Name = crew.Name
-		personCheck.KnowForDepartment = crew.KnownForDepartment
-		personCheck.Language = language
-		personCheck.Slug = slug.Make(personCheck.Name)
-		personCheck.SlugUrl = "person-" + strconv.Itoa(personCheck.Id)
-		person.PopulatePersonByLanguage(personCheck, language)
-		// personFindUpdate := person.GetPersonByIdAndLanguage(crew.Id, language)
+	// 	var personCheck person.Person
+	// 	personCheck.Id = crew.Id
+	// 	personCheck.Name = crew.Name
+	// 	personCheck.KnowForDepartment = crew.KnownForDepartment
+	// 	personCheck.Language = language
+	// 	personCheck.Slug = slug.Make(personCheck.Name)
+	// 	personCheck.SlugUrl = "person-" + strconv.Itoa(personCheck.Id)
+	// 	person.PopulatePersonByLanguage(personCheck, language)
+	// 	// personFindUpdate := person.GetPersonByIdAndLanguage(crew.Id, language)
 
-		// if personFindUpdate.Id == 0 {
-		// 	log.Println("TREAT PERSON CREW: ", personCheck.Id)
-		// 	person.PopulatePersonByLanguage(personCheck, language)
-		// 	// person.InsertPerson(personCheck)
-		// }
-		crew.OriginalName = ""
-	}
-	// FINAL TRATAMENTO DAS PESSOAS DO CAST E CREW
+	// 	// if personFindUpdate.Id == 0 {
+	// 	// 	log.Println("TREAT PERSON CREW: ", personCheck.Id)
+	// 	// 	person.PopulatePersonByLanguage(personCheck, language)
+	// 	// 	// person.InsertPerson(personCheck)
+	// 	// }
+	// 	crew.OriginalName = ""
+	// }
+	// // FINAL TRATAMENTO DAS PESSOAS DO CAST E CREW
 
 	itemFind := GetMovieByIdAndLanguage(itemObj.Id, language)
 
 	if itemFind.Id == 0 {
 		log.Println("INSERT MOVIE: ", itemObj.Id)
 		InsertMovie(language, itemObj)
+	} else {
+		log.Println("MOVIE ALREADY INSERTED: ", itemObj.Id)
 	}
 }
 
@@ -138,7 +141,7 @@ func GetAll(skip int64, limit int64) []Movie {
 	defer client.Disconnect(ctx)
 
 	optionsFind := options.Find().SetLimit(limit).SetSkip(skip)
-	cur, err := client.Database(os.Getenv("MONGO_DATABASE")).Collection("movie").Find(context.TODO(), bson.M{}, optionsFind)
+	cur, err := client.Database(os.Getenv("MONGO_DATABASE")).Collection(movieCollection).Find(context.TODO(), bson.M{}, optionsFind)
 	if err != nil {
 		log.Println(err)
 	}
@@ -164,7 +167,7 @@ func GetCountAll() int64 {
 	defer cancel()
 	defer client.Disconnect(ctx)
 
-	count, err := client.Database(os.Getenv("MONGO_DATABASE")).Collection("movie").CountDocuments(context.TODO(), bson.M{})
+	count, err := client.Database(os.Getenv("MONGO_DATABASE")).Collection(movieCollection).CountDocuments(context.TODO(), bson.M{})
 	if err != nil {
 		log.Println(err)
 	}
@@ -179,7 +182,7 @@ func GetMovieByIdAndLanguage(id int, language string) Movie {
 	defer client.Disconnect(ctx)
 
 	var item Movie
-	client.Database(os.Getenv("MONGO_DATABASE")).Collection("movie").FindOne(context.TODO(), bson.M{"id": id, "language": language}).Decode(&item)
+	client.Database(os.Getenv("MONGO_DATABASE")).Collection(movieCollection).FindOne(context.TODO(), bson.M{"id": id, "language": language}).Decode(&item)
 
 	return item
 }
@@ -190,7 +193,7 @@ func InsertMovie(language string, itemInsert Movie) interface{} {
 	defer cancel()
 	defer client.Disconnect(ctx)
 
-	result, err := client.Database(os.Getenv("MONGO_DATABASE")).Collection("movie").InsertOne(context.TODO(), itemInsert)
+	result, err := client.Database(os.Getenv("MONGO_DATABASE")).Collection(movieCollection).InsertOne(context.TODO(), itemInsert)
 	if err != nil {
 		log.Println("EERRORRR")
 		log.Println(err)
@@ -205,7 +208,7 @@ func InsertMany(movies []interface{}) interface{} {
 	defer cancel()
 	defer client.Disconnect(ctx)
 
-	result, err := client.Database(os.Getenv("MONGO_DATABASE")).Collection("movie").InsertMany(context.TODO(), movies)
+	result, err := client.Database(os.Getenv("MONGO_DATABASE")).Collection(movieCollection).InsertMany(context.TODO(), movies)
 	if err != nil {
 		log.Println("EERRORRR")
 		log.Println(err)
@@ -222,7 +225,7 @@ func Update(movie Movie, language string) {
 	defer cancel()
 	defer client.Disconnect(ctx)
 
-	client.Database(os.Getenv("MONGO_DATABASE")).Collection("movie").UpdateOne(context.TODO(), bson.M{"id": movie.Id, "language": language}, bson.M{
+	client.Database(os.Getenv("MONGO_DATABASE")).Collection(movieCollection).UpdateOne(context.TODO(), bson.M{"id": movie.Id, "language": language}, bson.M{
 		"$set": movie,
 	})
 }
@@ -234,7 +237,7 @@ func UpdateMany(movies []Movie, language string) {
 	defer client.Disconnect(ctx)
 
 	for _, movie := range movies {
-		client.Database(os.Getenv("MONGO_DATABASE")).Collection("movie").UpdateOne(context.TODO(), bson.M{"id": movie.Id, "language": language}, bson.M{
+		client.Database(os.Getenv("MONGO_DATABASE")).Collection(movieCollection).UpdateOne(context.TODO(), bson.M{"id": movie.Id, "language": language}, bson.M{
 			"$set": movie,
 		})
 	}
