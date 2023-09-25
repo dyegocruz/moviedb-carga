1877e05d20094ac80ef90eac48fcc5b7c92b49d5
Dyego Cruz <dyegocruz@gmail.com>
2023-09-25 14:40:00 -0300

    Change skip strategy to get mongodb data

--- carga/carga.service.go
+++ carga/carga.service.go
@@ -426,9 +426,9 @@ func ElasticChargeInsert(indexName string, interval int64, mapping string, bulkA
 }
 
 func ElasticGeneralCharge() {
-	go ElasticChargeInsert("series", 500, INDEX_MAPPING_SERIES, 1000)
+	ElasticChargeInsert("series", 500, INDEX_MAPPING_SERIES, 1000)
 	ElasticChargeInsert("movies", 500, INDEX_MAPPING_MOVIES, 1000)
-	go ElasticChargeInsert("persons", 500, INDEX_MAPPING_PERSONS, 1000)
+	ElasticChargeInsert("persons", 500, INDEX_MAPPING_PERSONS, 1000)
 	ElasticChargeInsert("series-episodes", 500, INDEX_MAPPING_SERIES_EPISODE, 1000)
 	// ElasticChargeInsert("series", 1000, INDEX_MAPPING_SERIES, 1000)
 	// ElasticChargeInsert("persons", 1000, INDEX_MAPPING_PERSONS, 1000)
