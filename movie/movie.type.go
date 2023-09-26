8049e69f64a391d2f2f4e7942b3151cb623641e5
Dyego Cruz <dyegocruz@gmail.com>
2023-09-26 14:55:01 -0300

    Removing some blank fields for ES charge and removind episodes charge

--- movie/movie.type.go
+++ movie/movie.type.go
@@ -28,12 +28,12 @@ type SpokenLanguages struct {
 
 type MovieCast struct {
 	// Adult              bool    `json:"adult"`
-	Gender             int     `json:"gender"`
+	Gender             int     `json:"gender,omitempty"`
 	Id                 int     `json:"id" bson:"id"`
-	KnownForDepartment string  `json:"known_for_department"`
+	KnownForDepartment string  `json:"known_for_department,omitempty"`
 	Name               string  `json:"name"`
-	OriginalName       string  `json:"original_name"`
-	Popularity         float64 `json:"popularity"`
+	OriginalName       string  `json:"original_name,omitempty"`
+	Popularity         float64 `json:"popularity,omitempty"`
 	ProfilePath        string  `json:"profile_path"`
 	Character          string  `json:"character" bson:"character"`
 	Order              int     `json:"order"`
@@ -43,14 +43,14 @@ type MovieCast struct {
 
 type MovieCrew struct {
 	// Adult              bool    `json:"adult"`
-	Gender             int     `json:"gender"`
+	Gender             int     `json:"gender,omitempty"`
 	Id                 int     `json:"id" bson:"id"`
-	KnownForDepartment string  `json:"known_for_department"`
+	KnownForDepartment string  `json:"known_for_department,omitempty"`
 	Name               string  `json:"name"`
-	OriginalName       string  `json:"original_name"`
-	Popularity         float64 `json:"popularity"`
+	OriginalName       string  `json:"original_name,omitempty"`
+	Popularity         float64 `json:"popularity,omitempty"`
 	ProfilePath        string  `json:"profile_path"`
-	Department         string  `json:"department" bson:"department"`
+	Department         string  `json:"department" bson:"department,omitempty"`
 	Job                string  `json:"job" bson:"job"`
 	// CreditID           string  `json:"credit_id"`
 }
@@ -75,17 +75,17 @@ type Movie struct {
 	Runtime             int                   `json:"runtime,omitempty" bson:"runtime"`
 	OriginalLanguage    string                `json:"original_language" bson:"original_language"`
 	OriginalTitle       string                `json:"original_title" bson:"original_title"`
-	GenreIds            []int                 `json:"genre_ids" bson:"genre_ids"`
+	GenreIds            []int                 `json:"genre_ids,omitempty" bson:"genre_ids"`
 	BackdropPath        string                `json:"backdrop_path" bson:"backdrop_path"`
 	Adult               bool                  `json:"adult" bson:"adult"`
 	Overview            string                `json:"overview" bson:"overview"`
 	PosterPath          string                `json:"poster_path" bson:"poster_path"`
 	MediaType           string                `json:"media_type" bson:"media_type"`
 	Language            string                `json:"language" bson:"language"`
-	SlugUrl             string                `json:"slugUrl" bson:"slugUrl"`
-	Slug                string                `json:"slug" bson:"slug"`
-	Updated             time.Time             `json:"updated" bson:"updated"`
-	UpdatedNew          string                `json:"updatedNew" bson:"updatedNew"`
+	SlugUrl             string                `json:"slugUrl,omitempty" bson:"slugUrl"`
+	Slug                string                `json:"slug,omitempty" bson:"slug"`
+	Updated             *time.Time            `json:"updated,omitempty" bson:"updated"`
+	UpdatedNew          string                `json:"updatedNew,omitempty" bson:"updatedNew"`
 	Genres              []Genres              `json:"genres" bson:"genres"`
 	ProductionCompanies []ProductionCompanie  `json:"production_companies" bson:"production_companies"`
 	ProductionCountries []ProductionCountries `json:"production_countries" bson:"production_countries"`
