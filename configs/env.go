edb480cd4dbc08df9883df2bb4be6c65c14d45aa
Dyego Cruz <dyegocruz@gmail.com>
2023-10-01 21:33:35 -0300

    Change mongodb config

---
+++ configs/env.go
@@ -0,0 +1,30 @@
+package configs
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func GetEnv() string {
+	return os.Getenv("GO_ENV")
+}
+
+func MongoURI() string {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	return os.Getenv("MONGO_URI")
+}
+
+func MongoDatabase() string {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	return os.Getenv("MONGO_DATABASE")
+}
