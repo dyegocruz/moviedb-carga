e9ceb1429deebc6e5a6a446341aa774e9d67e6d1
Dyego Cruz <dyegocruz@gmail.com>
2026-09-04 09:06:33 -0300

    feat: upgrader workers for now

--- start.server.sh
+++ start.server.sh
@@ -11,4 +11,4 @@ docker-compose config
 
 docker-compose up -d --build --force --remove-orphans
 
-docker-compose -f docker-compose-catalog-worker.yml up -d --build --scale catalog-process-worker=4
+docker-compose -f docker-compose-catalog-worker.yml up -d --build --scale catalog-process-worker=8
